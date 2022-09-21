b05dbed64f5a5d5b1c52772643ff994f13794726
LucasSlv <lucassilvax005x@gmail.com>
2022-09-21 15:43:03 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { worker } from './mocks/browser';
 
-worker.start()
+
+worker.start();
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
